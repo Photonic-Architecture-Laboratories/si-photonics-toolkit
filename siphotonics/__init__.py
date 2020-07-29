ecda0edb4eb53552fd6d09a53c5402f29d6ee097
Aycan Deniz Vit <avit16@ku.edu.tr>
2020-07-30 00:55:43 +0300

    Group Index function uploaded, coupling coefficient fix
    Former-commit-id: ace7b9d52b119d3f22f494229c0a47cdf9e57a09

--- siphotonics/__init__.py
+++ siphotonics/__init__.py
@@ -3,3 +3,4 @@ from siphotonics.neff_pol import polarization_frac
 from siphotonics.permittivity import perm_si
 from siphotonics.permittivity import perm_oxide
 from siphotonics.couplingc import couplingCoefficient
+from siphotonics.group_index import ng
---
+++ siphotonics/group_index.py
@@ -0,0 +1,22 @@
+import os
+from scipy import constants
+import siphotonics as sip
+import numpy as np
+import pickle
+
+user_dir = os.getcwd()
+os.chdir(os.path.join(os.path.dirname(__file__), "data"))
+with open(r"Si_Palik_Refractive_Index.pickle", "rb") as input_file:
+    refractive_index = pickle.load(input_file)
+os.chdir(user_dir)
+
+def ng(width, wavelength):
+    if wavelength > 1.7 or wavelength < 1.2:
+        raise ValueError("Wavelength must be between 1.2 and 1.7 microns.")
+    if width > 0.7 or width < 0.3:
+        raise ValueError("Width must be between 0.3 and 0.7 microns.")
+    if wavelength == 1.7:
+        n_g = sip.neff(width, wavelength,1) - wavelength * (sip.neff(width, wavelength,1) - sip.neff(width, wavelength-0.001,1))/(0.001)
+    else:
+        n_g = sip.neff(width, wavelength,1) - wavelength * (sip.neff(width, wavelength+0.001,1) - sip.neff(width, wavelength,1))/(0.001)
+    return n_g
