af8470a821a929c25e25028a7304f1e2ed23dd7b
Aycan Deniz Vit <avit16@ku.edu.tr>
2020-12-23 09:44:52 +0300

    Add comments to the trax_neff functions
    Former-commit-id: 7a38c45cce506bec8c62c0c87f09912209e08602

--- siphotonics/trax_neff.py
+++ siphotonics/trax_neff.py
@@ -15,11 +15,12 @@ os.chdir(user_dir)
 @jit
 def fast_neff(width, wl, mode):
     """
-
-    :param width:
-    :param wl:
-    :param mode:
-    :return:
+    Gets Effective Index value by using corresponding parameters. This is
+    a JAX compatible function. JIT is enabled.
+    :param width: Waveguide width in microns. (0.25 - 0.7)
+    :param wl: Wavelength in microns. (1.2 - 1.7)
+    :param mode: Mode number. (1 - 5)
+    :return: Effective Index value. Scalar
     """
     return ndimage.map_coordinates(trax_neff_array, [(width - 0.25) * 1000, (wl - 1.2) * 1000, mode - 1], order=1)
 
@@ -27,10 +28,13 @@ def fast_neff(width, wl, mode):
 @jit
 def grad_neff(width, wl, mode):
     """
-    
-    :param width:
-    :param wl:
-    :param mode:
-    :return:
+    Gets derivatives of Effective Index with respect to waveguide width and
+    wavelength.
+    :param width: Waveguide width in microns. (0.25 - 0.7)
+    :param wl: Wavelength in microns. (1.2 - 1.7)
+    :param mode: Mode number. (1 - 5)
+    :return: A tuple of DeviceArrays. First element corresponds to derivative
+    with respect to width. Last element is the derivative with respect to
+    wavelength
     """
     return fastmath.grad(fast_neff, (0, 1))(width, wl, mode.astype(float))
