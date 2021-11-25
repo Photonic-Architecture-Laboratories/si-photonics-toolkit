7922a52e2402da98a2cefdb2c19a14872babbb85
Aycan Deniz Vit <avit16@ku.edu.tr>
2021-11-25 19:05:44 +0300

    chore: remove asymmetric coupling coefficient file and function
    Former-commit-id: 03b93c8e4b1513f08016b3fe949fe093051abd2a

--- siphotonics/couplingc.py
+++ siphotonics/couplingc.py
@@ -1,7 +1,4 @@
 import os
-from scipy.interpolate import RegularGridInterpolator
-import numpy as np
-import pickle
 import h5py
 import jax
 from jax.scipy import ndimage
@@ -9,35 +6,11 @@ from jax import numpy as jnp
 
 user_dir = os.getcwd()
 os.chdir(os.path.join(os.path.dirname(__file__), "data"))
-with open(r"coupling_coefficient_TE0-TE0.pickle", "rb") as input_file:
-    couplingCoefficientArray = pickle.load(input_file)
 dc_mesh_2 = h5py.File('dc-mesh-2.mat', "r")
 dc_mesh_3 = h5py.File('dc-mesh-3.mat', "r")
 dc_mesh_4 = h5py.File('dc-mesh-4.mat', "r")
 wl_interval = jnp.linspace(1.2, 1.7, 101)
 os.chdir(user_dir)
-
-width1_array = np.linspace(300, 701, 41)
-width2_array = np.linspace(300, 701, 41)
-wavelengthArray = np.arange(1200, 1701, 10)
-gapArr = np.logspace(np.log(100), np.log(2001), num=150, endpoint=True, base=np.e, dtype=int)
-points = (width1_array, width2_array, gapArr, wavelengthArray)
-my_interp = RegularGridInterpolator(points, couplingCoefficientArray)
-
-
-def coupling_coefficient(width1, width2, gap, wavelength):
-    """
-
-    :param width1:
-    :param width2:
-    :param gap:
-    :param wavelength:
-    :return:
-    """
-    if gap >= 2:
-        return 0.00001
-    return my_interp([width2 * 1000, width1 * 1000, gap * 1000, wavelength * 1000])[0]
-    pass
 
 
 def through_power(wavelength, mesh=4):
