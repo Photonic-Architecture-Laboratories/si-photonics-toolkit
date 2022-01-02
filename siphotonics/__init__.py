da43e3cde8be305a8e1e52296e90b3fd720608df
Aycan Deniz Vit <avit16@ku.edu.tr>
2022-01-02 11:59:49 +0300

    feat: add new effective index functions
    Former-commit-id: fc6b44e98cef5f901538207ff49b589c1722fce0

--- siphotonics/__init__.py
+++ siphotonics/__init__.py
@@ -1,4 +1,4 @@
-from siphotonics.trax_neff import neff
+from siphotonics.trax_neff import neff, neff_te0, neff_te1, neff_te2, neff_tm0, neff_tm1
 from siphotonics.trax_neff import grad_neff
 from siphotonics.neff_pol import polarization_frac
 from siphotonics.permittivity import perm_si
