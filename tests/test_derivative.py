71773e1211a99d1e343218664c668076a9040bea
Aycan Deniz Vit <avit16@ku.edu.tr>
2022-01-02 12:18:36 +0300

    test: add new test cases
    Former-commit-id: 7060b7dad9681aaedd7d58ceb07a2b6ef13cdced

---
+++ tests/test_derivative.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pytest
+
+import siphotonics as sip
+
+def test_derivative():
+    x = np.linspace(0, 10, 400)
+    y = np.cos(2 * np.pi * x)
+
+    dydx = sip.derivative(y, order=1)
+    dydx2 = sip.derivative(y, order=2)
+    dydx3 = sip.derivative(y, order=3)
+    dydx4 = sip.derivative(y, order=4)
+
+    assert y.shape == dydx.shape
+    assert y.shape == dydx2.shape
+    assert y.shape == dydx3.shape
+    assert y.shape == dydx4.shape
+
+    with pytest.raises(Exception):
+        assert y.shape == sip.derivative(y, order=5)
