88b5f2816aefea7d4a40da52464dbdb53f5c08ce
Aycan Deniz Vit <avit16@ku.edu.tr>
2022-01-18 21:39:35 +0300

    chore: remove neff_pol and ring modules

--- siphotonics/__init__.py
+++ siphotonics/__init__.py
@@ -1,5 +1,3 @@
 from siphotonics import effective_index
 from siphotonics.group_index import group_index
-from siphotonics.neff_pol import polarization_frac
 from siphotonics.permittivity import perm_oxide, perm_si
-from siphotonics.ring import fsr
