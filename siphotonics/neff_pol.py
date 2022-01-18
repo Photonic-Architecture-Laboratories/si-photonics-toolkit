ae55cc3d262932c8f50f1092883211b7e714fa23
Aycan Deniz Vit <avit16@ku.edu.tr>
2022-01-18 10:16:49 +0300

    refactor: update multiplication factors in neff_pol

--- siphotonics/neff_pol.py
+++ siphotonics/neff_pol.py
@@ -34,25 +34,25 @@ def polarization_frac(width, wavelength, te_or_tm):
     :param te_or_tm:
     :return:
     """
-    width = width * 1000
+    width_nm = width * 1000
     wavelength_nm = wavelength * 1000
-    if not width_min * 1000 <= width <= width_max * 1000:
+    if not width_min <= width <= width_max:
         raise ValueError(f"Width must be between {width_min}-{width_max} micron")
-    if not wav_min * 1000 <= wavelength_nm <= wav_max * 1000:
+    if not wav_min <= wavelength <= wav_max:
         raise ValueError(f"wavelength must be between {wav_min}-{wav_max} micron")
 
     if isinstance(te_or_tm, str):
 
         if te_or_tm[0:2] == "te":
             if wavelength_nm in pol_te_list[int(te_or_tm[2:3])]:
-                if width in pol_te_list[int(te_or_tm[2:3])][wavelength_nm]:
-                    return pol_te_list[int(te_or_tm[2:3])][wavelength_nm][width]
+                if width_nm in pol_te_list[int(te_or_tm[2:3])][wavelength_nm]:
+                    return pol_te_list[int(te_or_tm[2:3])][wavelength_nm][width_nm]
         if te_or_tm[0:2] == "tm":
             if (
                 wavelength_nm in pol_tm_list[int(te_or_tm[2:3])]
-                and width in pol_tm_list[int(te_or_tm[2:3])][wavelength_nm]
+                and width_nm in pol_tm_list[int(te_or_tm[2:3])][wavelength_nm]
             ):
-                return pol_tm_list[int(te_or_tm[2:3])][wavelength_nm][width]
+                return pol_tm_list[int(te_or_tm[2:3])][wavelength_nm][width_nm]
 
         te_or_tm = te_or_tm.lower()
         last_index = 5
@@ -60,53 +60,53 @@ def polarization_frac(width, wavelength, te_or_tm):
         tm_number = 0
 
         for j, _f in enumerate(F):
-            if _f(wavelength_nm, width)[0] < 1.44:
+            if _f(wavelength_nm, width_nm)[0] < 1.44:
                 last_index = j
                 break
-            if P[j](wavelength_nm, width)[0] > 0.5:
+            if P[j](wavelength_nm, width_nm)[0] > 0.5:
                 te_number += 1
-            if P[j](wavelength_nm, width)[0] < 0.5:
+            if P[j](wavelength_nm, width_nm)[0] < 0.5:
                 tm_number += 1
 
         pol_te = []
         pol_tm = []
         for j in range(last_index):
-            if P[j](wavelength_nm, width)[0] > 0.50:
-                pol_te.append(P[j](wavelength_nm, width)[0])
+            if P[j](wavelength_nm, width_nm)[0] > 0.50:
+                pol_te.append(P[j](wavelength_nm, width_nm)[0])
             else:
-                pol_tm.append(P[j](wavelength_nm, width)[0])
+                pol_tm.append(P[j](wavelength_nm, width_nm)[0])
 
         if te_or_tm[0:2] == "te" and int(te_or_tm[2:3]) < te_number:
             ind = int(te_or_tm[2:3])
-            a_dict = {wavelength_nm: {width: pol_te[ind]}}
+            a_dict = {wavelength_nm: {width_nm: pol_te[ind]}}
             pol_te_list[ind].update(a_dict)
             return pol_te[ind]
         if not te_or_tm[0:2] == "tm":
             raise ValueError(
-                "There is no TE" + te_or_tm[2:3] + " for wavelength: " + str(wavelength_nm) + " & Width: " + str(width)
+                "There is no TE" + te_or_tm[2:3] + " for wavelength: " + str(wavelength_nm) + " & Width: " + str(width_nm)
             )
 
         if te_or_tm[0:2] == "tm" and int(te_or_tm[2:3]) < tm_number:
             ind = int(te_or_tm[2:3])
-            a_dict = {wavelength_nm: {width: pol_tm[ind]}}
+            a_dict = {wavelength_nm: {width_nm: pol_tm[ind]}}
             pol_tm_list[ind].update(a_dict)
             return pol_tm[ind]
 
         if not te_or_tm[0:2] == "te":
             raise ValueError(
-                "There is no TM" + te_or_tm[2:3] + " for wavelength: " + str(wavelength_nm) + " & Width: " + str(width)
+                "There is no TM" + te_or_tm[2:3] + " for wavelength: " + str(wavelength_nm) + " & Width: " + str(width_nm)
             )
 
     if isinstance(te_or_tm, int):
         if not 1 <= te_or_tm <= 5:
             raise ValueError("Mode should be between 1-5")
         if te_or_tm == 1:
-            return P[0](wavelength_nm, width)[0]
+            return P[0](wavelength_nm, width_nm)[0]
         if te_or_tm == 2:
-            return P[1](wavelength_nm, width)[0]
+            return P[1](wavelength_nm, width_nm)[0]
         if te_or_tm == 3:
-            return P[2](wavelength_nm, width)[0]
+            return P[2](wavelength_nm, width_nm)[0]
         if te_or_tm == 4:
-            return P[3](wavelength_nm, width)[0]
-        return P[4](wavelength_nm, width)[0]
+            return P[3](wavelength_nm, width_nm)[0]
+        return P[4](wavelength_nm, width_nm)[0]
     return -1
