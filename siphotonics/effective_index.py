2e6001b7ce5bea532751caaab1f00a5f1f37ead5
Aycan Deniz Vit <avit16@ku.edu.tr>
2022-01-31 13:07:40 +0300

    refactor: move data read to read_data module

--- siphotonics/effective_index.py
+++ siphotonics/effective_index.py
@@ -3,48 +3,10 @@ from jax import jit
 from jax.scipy import ndimage
 from trax import fastmath
 
+from siphotonics.read_data import neff_data, wav_min, wav_size, wav_max, width_min, width_size, width_max, \
+    effective_index_te0, width_min_te0, width_size_te0, width_max_te0, wav_size_te0, wav_min_te0, wav_max_te0, \
+    effective_index_tm0, effective_index_te1, effective_index_tm1, effective_index_te2
 from siphotonics.util import _read_effective_index
-
-neff_data, width_data, wav_data = _read_effective_index(
-    "neff_fitted_Si_fitted_SiO2_width_240_5_700_wav_1200_0p1_1700.csv"
-)
-
-wav_size = wav_data.shape[0]
-wav_min = np.min(wav_data)
-wav_max = np.max(wav_data)
-
-width_size = width_data.shape[0]
-width_min = np.min(width_data)
-width_max = np.max(width_data)
-
-
-effective_index_te0, width_data_te0, wav_data_te0 = _read_effective_index(
-    "neff_te0_fitted_Si_fitted_SiO2_width_240_5_700_wav_1200_0p1_1700.csv"
-)
-
-effective_index_tm0, _, _ = _read_effective_index(
-    "neff_tm0_fitted_Si_fitted_SiO2_width_240_5_700_wav_1200_0p1_1700.csv"
-)
-
-effective_index_te1, _, _ = _read_effective_index(
-    "neff_te1_fitted_Si_fitted_SiO2_width_240_5_700_wav_1200_0p1_1700.csv"
-)
-
-effective_index_tm1, _, _ = _read_effective_index(
-    "neff_tm1_fitted_Si_fitted_SiO2_width_240_5_700_wav_1200_0p1_1700.csv"
-)
-
-effective_index_te2, _, _ = _read_effective_index(
-    "neff_te2_fitted_Si_fitted_SiO2_width_240_5_700_wav_1200_0p1_1700.csv"
-)
-
-width_size_te0 = width_data_te0.shape[0]
-width_min_te0 = np.min(width_data_te0)
-width_max_te0 = np.max(width_data_te0)
-
-wav_size_te0 = wav_data_te0.shape[0]
-wav_min_te0 = np.min(wav_data_te0)
-wav_max_te0 = np.max(wav_data_te0)
 
 
 @jit
