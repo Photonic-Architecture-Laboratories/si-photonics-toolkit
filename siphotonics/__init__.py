746cf5cc5bcb3022b7a9d8f70242f0f9692af61b
Aycan Deniz Vit <aycandv@gmail.com>
2022-12-17 13:12:30 +0300

    fix(version): fix version build errors

--- siphotonics/__init__.py
+++ siphotonics/__init__.py
@@ -1,4 +1,5 @@
+from siphotonics import __version__
+
 from siphotonics import effective_index
 from siphotonics.group_index import group_index
 from siphotonics.permittivity import perm_oxide, perm_si
-
