4a5341c97aa832306fbc8c16c0e94799b2bbdb1f
Aycan Deniz Vit <aycandv@gmail.com>
2023-04-14 11:10:48 +0300

    docs(conf): update author names

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 pdf_documents = [
-    ("index", "rst2pdf", "Sample rst2pdf doc", "A D Vit"),
+    ("index", "rst2pdf", "Sample rst2pdf doc", "A D Vit, K Gorgulu, A N Amiri, E S Magden"),
 ]
 pdf_stylesheets = ["sphinx", "kerning", "a4"]
 pdf_break_level = 0
